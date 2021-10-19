0f93b32f50ed6e9f3530087a731dca4497c9a264
Samuel <samuel@mknd-records.com>
2021-10-19 12:49:18 +0200

    Prepare Main

--- src/ch/comem/dulex/series3/series3.java
+++ src/ch/comem/dulex/series3/series3.java
@@ -6,5 +6,17 @@ import java.util.Scanner;
 public class series3 {
 
     public static void main(String[] args) {
-    }
+        Scanner input = new Scanner(System.in);
+        int intChoice;
+
+
+        while (true) {
+            System.out.print("\n\nQuel exercice voulez-vous exécuter ? : ");
+            intChoice = input.nextInt();
+
+            switch (intChoice) {
+                case 1 -> ...;
+                default -> System.out.println("L'exercice n'esxiste pas ...");
+            }
+        }
 }
