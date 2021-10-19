20c3b000a7bf3eb137f2606724ba51ff3538a99e
Samuel <samuel@mknd-records.com>
2021-10-19 12:49:58 +0200

    Update Template

--- src/ch/comem/dulex/seriesX/seriesX.java
+++ src/ch/comem/dulex/seriesX/seriesX.java
@@ -4,4 +4,19 @@ import java.util.Scanner;
 
 
 public class seriesX {
-}
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int intChoice;
+
+
+        while (true) {
+            System.out.print("\n\nQuel exercice voulez-vous exécuter ? : ");
+            intChoice = input.nextInt();
+
+            switch (intChoice) {
+                case 1 -> ...;
+                default -> System.out.println("L'exercice n'esxiste pas ...");
+            }
+        }
+    }
