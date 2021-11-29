3d3edf29fe4889cfbea9de5d10f74d7c47d1c6c6
Samuel <samuel@mknd-records.com>
2021-11-29 13:38:21 +0100

    Exercice 3, 4 et 5

---
+++ src/ch/comem/dulex/series5/exercice4.java
@@ -0,0 +1,11 @@
+package ch.comem.dulex.series5;
+
+public class exercice4 {
+    public static void main(String[] args) {
+        String character = "A";
+        System.out.println(lowerCase(character));
+    }
+    private static String lowerCase(String character) {
+        return character.toLowerCase();
+    }
+}
